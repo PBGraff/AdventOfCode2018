f88971a70509f874abd0a7a4f5749070d1d60a53
Philip Graff <Philip.Graff@jhuapl.edu>
2019-12-17 10:14:54 -0500

    Last stuff

---
+++ day06/part1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# with open('input.txt', 'r') as f, open('input_processed.txt', 'w') as g:
+# 	for line in f:
+# 		g.write(line.replace(' ', ''))
+
+locs = pd.read_csv('input_processed.txt')
+
+
